3374065fce623595fe45605a03c6ae7e2cd47edf
Igor Zolotarev <izolotarev@hotmail.com>
2023-02-21 13:01:35 +0100

    feat: add header and forum section

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <div className="wrapper header__wrapper">
+          <div className="logo header__logo">
+            <div className="logo__picture"></div>
+            <div className="logo__wrapper">
+              <div className="logo__name-main">Security</div>
+              <div className="logo__name-sec">Forum</div>
+            </div>
+          </div>
+          <nav className="navigation">
+            <ul className="navigation__list">
+              <li className="navigation__item">
+                <a href="#forum" className="link-button link-button_visited">
+                  О мероприятии
+                </a>
+              </li>
+              <li className="navigation__item">
+                <a href="#speakers" className="link-button">
+                  Спикеры
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="user-block">
+            <a className="user-block__link" href="#signup">
+              Регистрация
+            </a>
+          </div>
+        </div>
       </header>
-    </div>
+      <main>
+        <section className="forum" id="forum">
+          <div className="wrapper forum__wrapper">
+            <h1 className="heading-1 forum__heading-main">Форум Microsoft</h1>
+            <h2 className="heading-1 forum__heading-sec">«Finance industry trust in AI and IT-Security»</h2>
+            <div className="forum__description">Масштабная кибератака и устаревшее оборудование может привести к потере клиентов, ухудшить отношения с партнёрами и учредителями, а невыполнение требований законодательства может привести к существенным штрафам и даже к блокировке ресурсов</div>
+            <div className="forum__tooltip">Узнайте как этого избежать, регистрируйтесь на наш форум</div>
+            <div className="user-block">
+              <a className="user-block__link" href="#signup">
+                Регистрация
+              </a>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer></footer>
+    </>
   );
 }
 
