b5905b36d5ae71ad53bfe2d4ca1e066ab5ae5b24
Igor Zolotarev <izolotarev@hotmail.com>
2023-02-22 11:54:41 +0100

    feat: add speakers and footer sections

--- src/App.tsx
+++ src/App.tsx
@@ -47,8 +47,201 @@ function App() {
             </div>
           </div>
         </section>
+        <section className="speakers" id="speakers">
+          <div className="wrapper">
+            <h2 className="heading-2 speakers__title">Спикеры нашего форума</h2>
+              <div className="layout-3-column speakers-wrapper">
+                <div className="speakers__person">
+                  <img
+                    className="speakers__person-img"
+                    src="assets/img/girl.jpg"
+                    alt="speaker"
+                  />
+                  <h3 className="heading-3 speakers__name">Кристина Боровикова</h3>
+                  <div className="speakers__line"></div>
+                  <div className="speakers__person-desc">
+                    Партнёр, отвечающий за услуги по GDPR, являющийся Microsoft Data Privacy Officer в Ernst & Young во Франции и части EU
+                  </div>
+                  <a className="speakers__detailed" href="/#">Подробнее</a>
+                </div>
+                <div className="speakers__person">
+                  <img
+                    className="speakers__person-img"
+                    src="assets/img/girl.jpg"
+                    alt="speaker"
+                  />
+                  <h3 className="heading-3 speakers__name">Кристина Боровикова</h3>
+                  <div className="speakers__line"></div>
+                  <div className="speakers__person-desc">
+                    Партнёр, отвечающий за услуги по GDPR, являющийся Microsoft Data Privacy Officer в Ernst & Young во Франции и части EU
+                  </div>
+                  <a className="speakers__detailed" href="/#">Подробнее</a>
+                </div>
+                <div className="speakers__person">
+                  <img
+                    className="speakers__person-img"
+                    src="assets/img/girl.jpg"
+                    alt="speaker"
+                  />
+                  <h3 className="heading-3 speakers__name">Кристина Боровикова</h3>
+                  <div className="speakers__line"></div>
+                  <div className="speakers__person-desc">
+                    Партнёр, отвечающий за услуги по GDPR, являющийся Microsoft Data Privacy Officer в Ernst & Young во Франции и части EU
+                  </div>
+                  <a className="speakers__detailed" href="/#">Подробнее</a>
+                </div>
+                <div className="speakers__person">
+                  <img
+                    className="speakers__person-img"
+                    src="assets/img/girl.jpg"
+                    alt="speaker"
+                  />
+                  <h3 className="heading-3 speakers__name">Кристина Боровикова</h3>
+                  <div className="speakers__line"></div>
+                  <div className="speakers__person-desc">
+                    Партнёр, отвечающий за услуги по GDPR, являющийся Microsoft Data Privacy Officer в Ernst & Young во Франции и части EU
+                  </div>
+                  <a className="speakers__detailed" href="/#">Подробнее</a>
+                </div>
+                <div className="speakers__person">
+                  <img
+                    className="speakers__person-img"
+                    src="assets/img/girl.jpg"
+                    alt="speaker"
+                  />
+                  <h3 className="heading-3 speakers__name">Кристина Боровикова</h3>
+                  <div className="speakers__line"></div>
+                  <div className="speakers__person-desc">
+                    Партнёр, отвечающий за услуги по GDPR, являющийся Microsoft Data Privacy Officer в Ernst & Young во Франции и части EU
+                  </div>
+                  <a className="speakers__detailed" href="/#">Подробнее</a>
+                </div>
+                <div className="speakers__person">
+                  <img
+                    className="speakers__person-img"
+                    src="assets/img/girl.jpg"
+                    alt="speaker"
+                  />
+                  <h3 className="heading-3 speakers__name">Кристина Боровикова</h3>
+                  <div className="speakers__line"></div>
+                  <div className="speakers__person-desc">
+                    Партнёр, отвечающий за услуги по GDPR, являющийся Microsoft Data Privacy Officer в Ernst & Young во Франции и части EU
+                  </div>
+                  <a className="speakers__detailed" href="/#">Подробнее</a>
+                </div>
+                <div className="speakers__person">
+                  <img
+                    className="speakers__person-img"
+                    src="assets/img/girl.jpg"
+                    alt="speaker"
+                  />
+                  <h3 className="heading-3 speakers__name">Кристина Боровикова</h3>
+                  <div className="speakers__line"></div>
+                  <div className="speakers__person-desc">
+                    Партнёр, отвечающий за услуги по GDPR, являющийся Microsoft Data Privacy Officer в Ernst & Young во Франции и части EU
+                  </div>
+                  <a className="speakers__detailed" href="/#">Подробнее</a>
+                </div>
+                <div className="speakers__person">
+                  <img
+                    className="speakers__person-img"
+                    src="assets/img/girl.jpg"
+                    alt="speaker"
+                  />
+                  <h3 className="heading-3 speakers__name">Кристина Боровикова</h3>
+                  <div className="speakers__line"></div>
+                  <div className="speakers__person-desc">
+                    Партнёр, отвечающий за услуги по GDPR, являющийся Microsoft Data Privacy Officer в Ernst & Young во Франции и части EU
+                  </div>
+                  <a className="speakers__detailed" href="/#">Подробнее</a>
+                </div>
+                <div className="speakers__person">
+                  <img
+                    className="speakers__person-img"
+                    src="assets/img/girl.jpg"
+                    alt="speaker"
+                  />
+                  <h3 className="heading-3 speakers__name">Кристина Боровикова</h3>
+                  <div className="speakers__line"></div>
+                  <div className="speakers__person-desc">
+                    Партнёр, отвечающий за услуги по GDPR, являющийся Microsoft Data Privacy Officer в Ernst & Young во Франции и части EU
+                  </div>
+                  <a className="speakers__detailed" href="/#">Подробнее</a>
+                </div>
+              </div>
+          </div>
+        </section>
       </main>
-      <footer></footer>
+      <footer className="footer">
+        <div className="wrapper footer__wrapper">
+          <div className="logo footer__logo">
+            <div className="logo__picture"></div>
+            <div className="logo__wrapper">
+              <div className="logo__name-main">Security</div>
+              <div className="logo__name-sec">Forum</div>
+            </div>
+          </div>
+          <div className="footer__contacts">
+            <ul className="footer__contacts-list">
+              <li className="footer__contacts-item">
+                <a className="footer__contacts-link" href="/#">Политика конфиденциальности</a>
+              </li>
+              <li className="footer__contacts-item">
+                <a className="footer__contacts-link" href="/#">Публичная оферта</a>
+              </li>
+              <li className="footer__contacts-item">
+                <a className="footer__contacts-link" href="tel:79121234567">+7 912 123-45-67</a>
+              </li>
+              <li className="footer__contacts-item">
+                <a className="footer__contacts-link" href="mailto:about@microsoftforum.com">about@microsoftforum.com</a>
+              </li>
+            </ul>
+          </div>
+          <nav className="footer__nav">
+            <ul className="footer__nav-list">
+              <li className="footer__nav-item">
+                <a className="footer__nav-link" href="#forum">О мероприятии</a>
+              </li>
+              <li className="footer__nav-item">
+                <a className="footer__nav-link" href="/#">Темы</a>
+              </li>
+              <li className="footer__nav-item">
+                <a className="footer__nav-link" href="/#">Расписание</a>
+              </li>
+              <li className="footer__nav-item">
+                <a className="footer__nav-link" href="#speakers">Спикеры</a>
+              </li>
+              <li className="footer__nav-item">
+                <a className="footer__nav-link" href="/#">Контакты</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="footer__social-links">
+            <ul className="social-list">
+              <li>
+                <a href="/#" aria-label="Vkontakte">
+                  <span className="icon-social icon-social_vkontakte"></span>
+                </a>
+              </li>
+              <li>
+                <a href="/#" aria-label="Facebook">
+                  <span className="icon-social icon-social_facebook"></span>
+                </a>
+              </li>
+              <li>
+                <a href="/#" aria-label="Instagram">
+                  <span className="icon-social icon-social_instagram"></span>
+                </a>
+              </li>
+              <li>
+                <a href="/#" aria-label="Telegram">
+                  <span className="icon-social icon-social_telegram"></span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
